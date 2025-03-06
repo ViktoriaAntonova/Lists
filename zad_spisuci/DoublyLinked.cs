6e54353eacd30c451b053825bbe0dc2f0e2fa308
MEGA <MEGA@gmail.com>
2025-03-06 18:19:24 +0200

    Add project files.

---
+++ zad_spisuci/DoublyLinked.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace zad_spisuci
+{
+    internal class DoublyLinked<T>
+    {
+        private DoublyNode<T> head;
+        private DoublyNode<T> tail;
+        public void Append(T data)
+        {
+            DoublyNode<T> newNode = new DoublyNode<T>(data);
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+            DoublyNode<T> temp = head;
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+            temp.Next = newNode;
+            newNode.Previous = temp;
+        }
+        public DoublyLinked()
+        {
+            head = null;
+            tail = null;
+        }
+        public void Reverse()
+        {
+            DoublyNode<T> curr = head;
+            DoublyNode<T> prev = null;
+
+            while (curr != null)
+            {
+                prev = curr.Previous;
+                curr.Previous = curr.Next;
+                curr.Next = prev;
+                curr = curr.Previous;
+            }
+
+            if (prev != null)
+            {
+                head = prev.Previous;
+            }
+        }
+        public void PrintList()
+        {
+            DoublyNode<T> current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + "<->");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
---
+++ zad_spisuci/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace zad_spisuci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinked<int> d = new DoublyLinked<int>();
+            d.Append(1);
+            d.Append(2);
+            d.Append(3);
+            d.Append(4);
+            d.Append(5);
+            Console.WriteLine("Original List:");
+            d.PrintList();
+            d.Reverse();
+            Console.WriteLine("Reversed List:");
+            d.PrintList();
+        }
+    }
+}
