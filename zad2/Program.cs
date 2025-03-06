2666fa513648430014c1eb3e4406111b1b61c5a4
MEGA <MEGA@gmail.com>
2025-03-06 18:37:43 +0200

    zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyList<int> list = new SinglyList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.PrintList();
+            Node<int> middle = list.FindMiddle();
+            Console.WriteLine("Средният възел е: " + middle.Data);
+        }
+    }
+}
---
+++ zad2/SinglyList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace zad2
+{
+    internal class SinglyList<T>
+    {
+        private Node<T> head;
+        public SinglyList()
+        {
+            head = null;
+        }
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newNode;
+            }
+        }
+        public Node<T> FindMiddle()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            Node<T> slow_ptr = head;
+            Node<T> fast_ptr = head;
+
+            while (fast_ptr != null && fast_ptr.Next != null)
+            {
+                slow_ptr = slow_ptr.Next;
+                fast_ptr = fast_ptr.Next.Next;
+            }
+            return slow_ptr;
+        }
+        public void PrintList()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + "->");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
